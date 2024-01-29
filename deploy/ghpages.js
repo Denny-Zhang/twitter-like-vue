4de4095cba5d661f67a7a5a746c7aef5d6be05f8
Denny <action79863@gmail.com>
2024-01-29 13:45:58 +0800

    feat: deploy github page

---
+++ deploy/ghpages.js
@@ -0,0 +1,25 @@
+// place at : deploy/ghpages.js
+// you can see more info at https://github.com/tschaub/gh-pages
+
+// refer to https://hackmd.io/@c36ICNyhQE6-iTXKxoIocg/rkJxgxhDI
+// import path, { resolve } from 'path'
+import { publish } from 'gh-pages'
+// import { fileURLToPath } from 'url'
+
+// const __filename = fileURLToPath(import.meta.url)
+// const __dirname = path.dirname(__filename)
+
+const options = {
+  branch: 'gh-pages',
+  repo: 'https://github.com/Denny-Zhang/vue-social-media.git' // project github repo
+}
+
+const callback = (err) => {
+  if (err) console.error(err)
+  else console.log('publish success')
+}
+
+/**
+ * This task pushes to the `master` branch of the configured `repo`.
+ */
+publish('dist', options, callback)
--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/vue-social-media/', // for github page
   plugins: [
     vue(),
     vueJsx(),
