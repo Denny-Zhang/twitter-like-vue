ef9a9a725ad304d4dc78cea5f28f87f8b3c3497b
Denny <action79863@gmail.com>
2024-02-13 16:10:48 +0800

    chore: change App view

--- src/App.vue
+++ src/App.vue
@@ -1,84 +1,25 @@
-<script setup lang="ts">
-  import { RouterLink, RouterView } from 'vue-router'
-  import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <ConfigProvider :locale="getAntdLocale" :theme="isDark ? darkTheme : {}">
+    <AppProvider>
+      <RouterView />
+    </AppProvider>
+  </ConfigProvider>
 </template>
 
-<style scoped>
-  header {
-    max-height: 100vh;
-    line-height: 1.5;
-  }
+<script lang="ts" setup>
+  import { ConfigProvider } from 'ant-design-vue';
+  import { AppProvider } from '@/components/Application';
+  import { useTitle } from '@/hooks/web/useTitle';
+  import { useLocale } from '@/locales/useLocale';
 
-  .logo {
-    display: block;
-    margin: 0 auto 2rem;
-  }
+  import 'dayjs/locale/zh-cn';
+  import { useDarkModeTheme } from '@/hooks/setting/useDarkModeTheme';
 
-  nav {
-    width: 100%;
-    margin-top: 2rem;
-    font-size: 12px;
-    text-align: center;
-  }
+  // support Multi-language
+  const { getAntdLocale } = useLocale();
 
-  nav a.router-link-exact-active {
-    color: var(--color-text);
-  }
+  const { isDark, darkTheme } = useDarkModeTheme();
 
-  nav a.router-link-exact-active:hover {
-    background-color: transparent;
-  }
-
-  nav a {
-    display: inline-block;
-    padding: 0 1rem;
-    border-left: 1px solid var(--color-border);
-  }
-
-  nav a:first-of-type {
-    border: 0;
-  }
-
-  @media (min-width: 1024px) {
-    header {
-      display: flex;
-      place-items: center;
-      padding-right: calc(var(--section-gap) / 2);
-    }
-
-    .logo {
-      margin: 0 2rem 0 0;
-    }
-
-    header .wrapper {
-      display: flex;
-      place-items: flex-start;
-      flex-wrap: wrap;
-    }
-
-    nav {
-      margin-top: 1rem;
-      margin-left: -1rem;
-      padding: 1rem 0;
-      font-size: 1rem;
-      text-align: left;
-    }
-  }
-</style>
+  // Listening to page changes and dynamically changing site titles
+  useTitle();
+</script>
